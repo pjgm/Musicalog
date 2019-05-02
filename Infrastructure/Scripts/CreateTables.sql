483f81752c1a319f8373c253c5469949cd0b20b2
Paulo Martins <pj.martins@campus.fct.unl.pt>
2019-05-02 22:45:33 +0100

    Add another way to submit form (commented)

--- Infrastructure/Scripts/CreateTables.sql
+++ Infrastructure/Scripts/CreateTables.sql
@@ -18,12 +18,12 @@ CREATE TABLE Albums (
     Id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
     Name varchar(MAX) NOT NULL,
 	AlbumType int NOT NULL,
-	ArtistId int FOREIGN KEY REFERENCES Artists(ArtistId),
-	RecordLabelId int FOREIGN KEY REFERENCES RecordLabels(RecordLabelId),
+	ArtistId int FOREIGN KEY REFERENCES Artists(Id),
+	RecordLabelId int FOREIGN KEY REFERENCES RecordLabels(Id),
 );
 
 CREATE TABLE Inventory (
 	Id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
-    AlbumId int FOREIGN KEY REFERENCES Albums(AlbumId),
+    AlbumId int FOREIGN KEY REFERENCES Albums(Id),
 	Stock int NOT NULL
 );
